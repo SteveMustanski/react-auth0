a136d0fd49efa9a994c208ed29bae3298ac7df46
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-21 17:48:11 -0500

    fixed directory structure and bugs in auth0 setup

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -6,9 +6,13 @@ export default class Auth {
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-      redirctUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+      redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
       responseType: "token id_token",
       scope: "openid profile email"
     });
   }
+
+  login = () => {
+    this.auth0.authorize();
+  };
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <button onClick={this.props.auth.login}>Log In</button>
       </div>
     );
   }
