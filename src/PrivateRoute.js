2e0a0a523b86ea5a7afe0c15087e552088c09319
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-24 17:52:21 -0500

    switched PrivateRoute to use context api

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,34 +1,38 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import PropTypes from "prop-types";
+import AuthContext from "./AuthContext";
 
-function PrivateRoute({ component: Component, auth, scopes, ...rest }) {
+function PrivateRoute({ component: Component, scopes, ...rest }) {
   return (
-    <Route
-      {...rest}
-      render={props => {
-        // redirect to login if not already logged in
-        if (!auth.isAuthenticated()) return auth.login();
+    <AuthContext.Consumer>
+      {auth => (
+        <Route
+          {...rest}
+          render={props => {
+            // redirect to login if not already logged in
+            if (!auth.isAuthenticated()) return auth.login();
 
-        // display if suer doesn't have the correct scope
-        if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
-          return (
-            <h1>
-              Unauthorized -- You need to have the following scopes to view this
-              page: {scopes.join(",")}.
-            </h1>
-          );
-        }
-        // render the component
-        return <Component auth={auth} {...props} />;
-      }}
-    />
+            // display if suer doesn't have the correct scope
+            if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
+              return (
+                <h1>
+                  Unauthorized -- You need to have the following scopes to view
+                  this page: {scopes.join(",")}.
+                </h1>
+              );
+            }
+            // render the component
+            return <Component auth={auth} {...props} />;
+          }}
+        />
+      )}
+    </AuthContext.Consumer>
   );
 }
 
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
   scopes: PropTypes.array
 };
 
