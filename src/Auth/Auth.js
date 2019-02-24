0369c410115e9705bf77bf03e94edc338d39d3d4
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-24 18:20:11 -0500

    moved token information from localstoreage to memory per auth0 recommendations

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,6 +1,11 @@
 import auth0 from "auth0-js";
 
 const REDIRECT_ON_LOGIN = "redirect_on_login";
+// eslint-disable-next-line
+let _idToken = null;
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
 
 export default class Auth {
   constructor(history) {
@@ -49,35 +54,25 @@ export default class Auth {
 
   setSession = authResult => {
     //set access token expiry time
-    const expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
+    _expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
 
     // If there is a value on the 'scope' param in authResult, use it to set scopes
     // in the seesion.  If not, use the scopes as requested.  If there are no scopes requested
     // set scopes to nothing
 
-    const scopes = authResult.scope || this.requestedScopes || "";
+    _scopes = authResult.scope || this.requestedScopes || "";
 
     // storing these values in local storage because it is easier at this point
     // will address this later
-    localStorage.setItem("access_token", authResult.accessToken);
-    localStorage.setItem("id_token", authResult.idToken);
-    localStorage.setItem("expires_at", expiresAt);
-    localStorage.setItem("scopes", JSON.stringify(scopes));
+    _accessToken = authResult.accessToken;
+    _idToken = authResult.idToken;
   };
 
   isAuthenticated = () => {
-    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-    return new Date().getTime() < expiresAt;
+    return new Date().getTime() < _expiresAt;
   };
 
   logout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
-    localStorage.removeItem("scopes");
-    this.userProfile = null;
     // logs out of auth0 and clears the cookie
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -86,11 +81,10 @@ export default class Auth {
   };
 
   getAccessToken = () => {
-    const accessToken = localStorage.getItem("access_token");
-    if (!accessToken) {
+    if (!_accessToken) {
       throw new Error("No access token found.");
     }
-    return accessToken;
+    return _accessToken;
   };
 
   getProfile = cb => {
@@ -101,9 +95,7 @@ export default class Auth {
     });
   };
   userHasScopes(scopes) {
-    const grantedScopes = (
-      JSON.parse(localStorage.getItem("scopes")) || ""
-    ).split(" ");
+    const grantedScopes = (_scopes || "").split(" ");
     return scopes.every(scope => grantedScopes.includes(scope));
   }
 }
